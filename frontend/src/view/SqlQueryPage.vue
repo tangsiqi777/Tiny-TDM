715dc84f2bb57e2d42d5dd5ab461e9bb9536c5ac
tsq <2551999188@qq.com>
2024-05-11 10:51:55 +0800

    限制自定义sql返回显示数量

--- frontend/src/view/SqlQueryPage.vue
+++ frontend/src/view/SqlQueryPage.vue
@@ -36,9 +36,14 @@ function subSql() {
     }
     console.log(JSON.stringify(pageData))
     headList.value = (getHead(pageData))
-    pageDataList.value = restDataToJsonObj(pageData)
+
+    let originalArray = restDataToJsonObj(pageData)
+    if (originalArray && originalArray.length > 0) {
+      pageDataList.value = originalArray.slice(0, 1000);
+    } else {
+      pageDataList.value = []
+    }
     console.log("headList:" + JSON.stringify(headList.value))
-    console.log("pageDataList:" + JSON.stringify(pageDataList.value))
   })
 }
 
@@ -60,7 +65,7 @@ function convertArrayToObject(arr) {
     <div class="info-c">
       <a-alert>1. SQL需要带上数据库限定<br/>
         2. 对于有大写数据库名数据库表的SQL需要对该字段使用``转义<br/>
-        3. 为了避免卡顿请控制返回数量<br/>
+        3. 为了避免卡只显示返回前1000条数据<br/>
       </a-alert>
     </div>
     <div class="sql-input-c">
